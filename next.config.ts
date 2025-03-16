f4febfea273a9ae1731f0f646880e35c6bfb3e96
hstrejoluna <trejolunatutoriales@gmail.com>
2025-03-16 02:51:01 -0500

    feat: landing kickstart

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: [
+      'images.unsplash.com', 
+      'randomuser.me'
+    ],
+  },
   /* config options here */
 };
 
